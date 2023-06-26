159d9c3be9f2900c0d73d468d938fde77f528302
Vixcity <2091283625@qq.com>
2023-06-26 14:09:54 +0800

    封装组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,11 @@
 import React from "react";
-import type { MouseEvent } from "react";
-// @ts-ignore
-import logo from "./logo.svg";
 import "./App.css";
+import List1 from "List1";
 
 function App() {
-  const fn = (event: MouseEvent<HTMLButtonElement>) => {
-    event.preventDefault();
-    console.log("clicked...");
-  };
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>Hello World</p>
-        <button onClick={fn}>点击</button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-          style={{ backgroundColor: "white", color: "red" }}
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <>
+    <List1></List1>
+  </>;
 }
 
 export default App;
