296e67bbc82facbccd58c50e9a7c64a66886698d
Vixcity <2091283625@qq.com>
2023-07-05 10:21:48 +0800

    表单组件提交

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
 import React from "react";
-import { useGetInfo } from "./hooks/index";
-import { useTitle } from "ahooks";
+// import { useGetInfo } from "./hooks/index";
+// import { useTitle } from "ahooks";
 import "./App.scss";
 // import List1 from "List1";
-import List2 from "List2";
+// import List2 from "List2";
 // import ImmerDemo from "immerDemo";
 // import UseRefDemo from "useRefDemo";
 // import UseMemoDemo from "useMemoDemo";
 // import UseCallback from "useCallback";
 // import ClosureTrap from "closureTrap";
 // import StyleComponentsDemo from "styledComponentsDemo";
+import FormElement from "FormElementDemo";
 
 function App() {
-  const { loading, info } = useGetInfo();
-  useTitle("APP Page 2");
+  // const { loading, info } = useGetInfo();
+  // useTitle("APP Page 2");
+
   return (
     <>
-      <p>{loading ? "加载中" : info}</p>
-      <List2></List2>
+      <FormElement />
+      {/* <p>{loading ? "加载中" : info}</p> */}
+      {/* <List2></List2> */}
     </>
   );
 }
---
+++ src/FormElementDemo.tsx
@@ -0,0 +1,121 @@
+import React, { FC, useState, ChangeEvent } from "react";
+
+const Demo: FC = () => {
+  // const [text, setText] = useState<string>("hello");
+
+  // function handleChange(event: ChangeEvent<HTMLTextAreaElement>) {
+  //   setText(event.target.value);
+  // }
+
+  // function getHtml() {
+  //   return {
+  //     __html: text.replaceAll("\n", "<br />"),
+  //   };
+  // }
+
+  // const [gender, setGender] = useState("male");
+  // function handleChange(event: ChangeEvent<HTMLInputElement>) {
+  //   setGender(event.target.value);
+  // }
+
+  // const [checked, setChecked] = useState(false);
+  // function toggleChecked() {
+  //   setChecked(!checked);
+  // }
+
+  const [selectedCityList, setSelectedCityList] = useState<string[]>([]);
+  function handleCityChange(event: ChangeEvent<HTMLInputElement>) {
+    const city = event.target.value;
+    if (selectedCityList.includes(city)) {
+      // 移除
+      setSelectedCityList(
+        selectedCityList.filter((c) => {
+          if (c === city) return false;
+          return true;
+        })
+      );
+    } else {
+      // 添加
+      setSelectedCityList(
+        selectedCityList.concat(city)
+      );
+    }
+  }
+
+  return (
+    <>
+      {/* <input type="text" value={text} onChange={handleChange} /> */}
+
+      {/* <textarea
+        name=""
+        id=""
+        cols={30}
+        rows={10}
+        value={text}
+        onChange={handleChange}
+      ></textarea>
+      {text.replaceAll("\n", "<br />")}
+      <p dangerouslySetInnerHTML={getHtml()}></p>
+      <button onClick={() => console.log(text)}>打印</button>
+      <button onClick={() => setText("设置")}>设置</button> */}
+
+      {/* <label htmlFor="radio1">男</label>
+      <input
+        type="radio"
+        id="radio1"
+        name="gender"
+        value="male"
+        onChange={handleChange}
+        checked={gender === "male"}
+      />
+      <label htmlFor="radio2">女</label>
+      <input
+        type="radio"
+        id="radio2"
+        name="gender"
+        value="female"
+        onChange={handleChange}
+        checked={gender === "female"}
+      /> */}
+
+      {/* <label htmlFor="checkbox1">选中</label>
+      <input
+        type="checkbox"
+        id="checkbox1"
+        onChange={toggleChecked}
+        checked={checked}
+      />
+      {JSON.stringify(checked)} */}
+
+      <label htmlFor="checkbox1">北京</label>
+      <input
+        type="checkbox"
+        id="checkbox1"
+        value="beijing"
+        checked={selectedCityList.includes("beijing")}
+        onChange={handleCityChange}
+      />
+      <label htmlFor="checkbox2">上海</label>
+      <input
+        type="checkbox"
+        id="checkbox2"
+        value="shanghai"
+        checked={selectedCityList.includes("shanghai")}
+        onChange={handleCityChange}
+      />
+      <label htmlFor="checkbox3">杭州</label>
+      <input
+        type="checkbox"
+        id="checkbox3"
+        value="hangzhou"
+        checked={selectedCityList.includes("hangzhou")}
+        onChange={handleCityChange}
+      />
+      {JSON.stringify(selectedCityList)}
+      {/* 提交到表单上面去，可以通过这个方式提交 */}
+      <input type="hidden" name="cities" value={JSON.stringify(selectedCityList)}/>
+    </>
+  );
+};
+
+export default Demo;
